e7b5c6964ce73c497e304c42393bcc7618833eab
ao <38201574+ainneo@users.noreply.github.com>
2022-05-14 14:24:34 -0700

    Update Example4.spec.tsx

--- src/examples/Example4/Example4.spec.tsx
+++ src/examples/Example4/Example4.spec.tsx
@@ -3,6 +3,9 @@ import { mocked } from 'ts-jest/utils';
 import { MyDrawer } from '../Example3/Drawer';
 import { Example4 } from './Example4';
 
+//we are mocking what the jest.mocked module is returning by using mockImplementation
+//so instead of returning its true vaule: 'Hello Drawer Component!'
+//we are returning our mockImplementation: 'mocked: drawer'
 jest.mock('../Example3/Drawer');
 mocked(MyDrawer).mockImplementation(() => <div>mocked: drawer</div>);
 
